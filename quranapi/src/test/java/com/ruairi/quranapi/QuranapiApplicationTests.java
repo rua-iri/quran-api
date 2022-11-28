2d803558b15e282a808f2095b8d800c9f63a129b
rua-iri <rory.mcguigan@protonmail.com>
2022-11-28 15:00:14 +0000

    add base files for spring boot application

---
+++ quranapi/src/test/java/com/ruairi/quranapi/QuranapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruairi.quranapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
