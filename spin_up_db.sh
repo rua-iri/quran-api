98e9654f1dc7077add4ed1b2a0a97aa805048afb
rua-iri <rory.mcguigan@protonmail.com>
2025-03-22 21:26:30 +0300

    add: script to spin up postgresql database in docker

---
+++ spin_up_db.sh
@@ -0,0 +1,12 @@
+docker pull postgres
+
+docker run --name=quran_db \
+    -e POSTGRES_PASSWORD=postgres \
+    -e PGPORT=5434 -p 5433:5434 \
+    -e POSTGRES_DB=quran_db \
+    -v quran_db:/var/lib/postgresql/data \
+    -d postgres:latest
+
+docker exec quran_db psql -U postgres -c "CREATE DATABASE quran_db"
+
+psql -h localhost -p 5433 -U postgres quran_db < quran.sql
