30f6093c80efc3b972db05787a1112e6ce5498e9
rua-iri <rory.mcguigan@protonmail.com>
2025-03-23 17:02:38 +0300

    add: tailwindcss

--- quran_viewer/src/components/Dashboard.jsx
+++ quran_viewer/src/components/Dashboard.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 export default function Dashboard() {
   return (
     <div>
-      <h1>Quran Viewer</h1>
-      <hr />
-      <hr />
+      <h1 className="my-10">Quran Viewer</h1>
+      <hr className="my-1" />
+      <hr className="my-1" />
       <Outlet />
     </div>
   );
